0c870dffb50dd59aa5d8e0e278d241451316dcf2
Jochen Berger <fooberger@gmail.com>
2017-02-08 10:36:36 +0100

    add test for updatable api dependency, add another fix for Gradle 3.4

--- src/main/groovy/com/github/benmanes/gradle/versions/updates/Resolver.groovy
+++ src/main/groovy/com/github/benmanes/gradle/versions/updates/Resolver.groovy
@@ -171,6 +171,10 @@ class Resolver {
 
     Map<Coordinate.Key, Coordinate> coordinates = [:]
     Configuration copy = configuration.copyRecursive().setTransitive(false)
+    // https://github.com/ben-manes/gradle-versions-plugin/issues/127
+    if (copy.metaClass.respondsTo(copy, "setCanBeResolved", Boolean)) {
+      copy.setCanBeResolved(true)
+    }
     LenientConfiguration lenient = copy.resolvedConfiguration.lenientConfiguration
 
     Set<ResolvedDependency> resolved = lenient.getFirstLevelModuleDependencies(SATISFIES_ALL)
---
+++ src/test/groovy/com/github/benmanes/gradle/versions/JavaLibrarySpec.groovy
@@ -0,0 +1,64 @@
+package com.github.benmanes.gradle.versions;
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+
+import spock.lang.Specification
+
+public class JavaLibrarySpec extends Specification {
+
+  @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+  File buildFile
+
+  List<File> pluginClasspath
+
+  def setup() {
+    buildFile = testProjectDir.newFile('build.gradle')
+
+    def pluginClasspathResource = getClass().classLoader.findResource("plugin-classpath.txt")
+    if (pluginClasspathResource == null) {
+      throw new IllegalStateException("Did not find plugin classpath resource, run `testClasses` build task.")
+    }
+
+    pluginClasspath = pluginClasspathResource.readLines().collect { new File(it) }
+  }
+
+  def "Show updates for an api dependency in a java-library project"() {
+    given:
+    def mavenRepoUrl = getClass().getResource('/maven/').toURI()
+    def srdErrWriter = new StringWriter()
+
+    buildFile << """
+        plugins {
+          id 'com.github.ben-manes.versions'
+        }
+        apply plugin: 'java-library'
+
+        repositories {
+          maven {
+            url '${mavenRepoUrl}'
+          }
+        }
+
+        dependencies {
+          api 'com.google.inject:guice:2.0'
+        }
+
+
+        """
+
+    when:
+    def result = GradleRunner.create()
+        .withGradleVersion('3.4-rc-1')
+        .withProjectDir(testProjectDir.root)
+        .withArguments('dependencyUpdates')
+        .withPluginClasspath(pluginClasspath)
+        .forwardStdError(srdErrWriter)
+        .build()
+
+    then:
+    result.output.contains('com.google.inject:guice [2.0 -> 3.0]')
+    srdErrWriter.toString().empty
+  }
+}
