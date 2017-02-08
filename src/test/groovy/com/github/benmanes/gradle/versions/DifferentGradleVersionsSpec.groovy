ba4d411e955a4dff40a40c05c78585fed82fc515
jochenberger <fooberger@gmail.com>
2017-02-08 08:27:48 +0100

    Test against Gradle 3.4-rc-2

--- src/test/groovy/com/github/benmanes/gradle/versions/DifferentGradleVersionsSpec.groovy
+++ src/test/groovy/com/github/benmanes/gradle/versions/DifferentGradleVersionsSpec.groovy
@@ -73,6 +73,6 @@ public class DifferentGradleVersionsSpec extends Specification {
     srdErrWriter.toString().empty
 
     where:
-    gradleVersion << ['2.0', '3.3']
+    gradleVersion << ['2.0', '3.3', '3.4-rc-2']
   }
 }
