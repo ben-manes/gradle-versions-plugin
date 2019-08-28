a88153f4dafb7e297f9af65351049f079436e7c3
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2019-08-28 08:15:38 +0200

    Use delegate annotation for ComponentSelectionWithCurrent

--- src/main/groovy/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentSelectionWithCurrent.groovy
+++ src/main/groovy/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentSelectionWithCurrent.groovy
@@ -1,10 +1,10 @@
 package com.github.benmanes.gradle.versions.updates.resolutionstrategy
 
 import org.gradle.api.artifacts.ComponentSelection
-import org.gradle.api.artifacts.component.ModuleComponentIdentifier
 
 class ComponentSelectionWithCurrent {
 
+  @Delegate
   private ComponentSelection delegate
 
   private String current
@@ -14,15 +14,7 @@ class ComponentSelectionWithCurrent {
     this.current = current
   }
 
-  ModuleComponentIdentifier getCandidate() {
-    return delegate.getCandidate()
-  }
-
   String getCurrent() {
     return current
-  }
-
-  void reject(String reason) {
-    delegate.reject(reason)
   }
 }
