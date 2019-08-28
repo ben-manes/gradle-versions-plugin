558b0b654a8c3020a9e5beb3dd77859d4ede869d
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2019-08-28 08:20:13 +0200

    Use TupleConstructor annotation for ComponentSelectionWithCurrent

--- src/main/groovy/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentSelectionWithCurrent.groovy
+++ src/main/groovy/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentSelectionWithCurrent.groovy
@@ -1,18 +1,15 @@
 package com.github.benmanes.gradle.versions.updates.resolutionstrategy
 
+import groovy.transform.TupleConstructor
 import org.gradle.api.artifacts.ComponentSelection
 
+@TupleConstructor(includeFields=true)
 class ComponentSelectionWithCurrent {
 
   @Delegate
   private ComponentSelection delegate
 
   private String current
-
-  ComponentSelectionWithCurrent(ComponentSelection delegate, String current) {
-    this.delegate = delegate
-    this.current = current
-  }
 
   String getCurrent() {
     return current
