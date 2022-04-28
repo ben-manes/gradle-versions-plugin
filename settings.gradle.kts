21e46fbd156764d12e5d734f40213ba7d43138bc
Jared Burrows <jaredsburrows@gmail.com>
2022-04-28 12:24:25 -0400

    update github actions (#601)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,12 @@ plugins {
   `gradle-enterprise`
 }
 
+gradleEnterprise {
+  buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    publishAlways()
+  }
+}
+
 rootProject.name = "gradle-versions-plugin"
