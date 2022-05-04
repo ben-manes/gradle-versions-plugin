cd423b8a82349d2cb0629f6dc843dea638502bc6
Jared Burrows <jaredsburrows@gmail.com>
2022-05-03 23:21:01 -0400

    migrate reports to kotlin (#628)

--- api/src/main/kotlin/com/github/benmanes/gradle/versions/reporter/AbstractReporter.kt
+++ api/src/main/kotlin/com/github/benmanes/gradle/versions/reporter/AbstractReporter.kt
@@ -1,15 +1,20 @@
 package com.github.benmanes.gradle.versions.reporter
 
 import org.gradle.api.Project
+import java.io.PrintWriter
 
 /**
  * A base result object reporter for the dependency updates results.
  */
 abstract class AbstractReporter @JvmOverloads constructor(
   /** The project evaluated against. */
-  val project: Project,
+  open val project: Project,
   /** The revision strategy evaluated with. */
-  val revision: String,
+  open val revision: String,
   /** The gradle release channel to use for reporting. */
-  val gradleReleaseChannel: String,
+  open val gradleReleaseChannel: String,
 ) : Reporter
+
+fun Appendable.println(x: String) {
+  (this as PrintWriter).println(x)
+}
---
+++ api/src/main/kotlin/com/github/benmanes/gradle/versions/reporter/JsonReporter.kt
@@ -0,0 +1,34 @@
+package com.github.benmanes.gradle.versions.reporter
+
+import com.github.benmanes.gradle.versions.reporter.result.Result
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.gradle.api.Project
+
+/**
+ * A json reporter for the dependency updates results.
+ */
+class JsonReporter @JvmOverloads constructor(
+  override val project: Project,
+  override val revision: String,
+  override val gradleReleaseChannel: String,
+) : AbstractReporter(project, revision, gradleReleaseChannel) {
+  override fun write(target: Appendable, result: Result) {
+    val jsonAdapter = moshi
+      .adapter(Result::class.java)
+      .serializeNulls()
+      .indent(" ")
+    val json = jsonAdapter.toJson(result).trimMargin()
+    target.println(json)
+  }
+
+  override fun getFileExtension(): String {
+    return "json"
+  }
+
+  companion object {
+    private val moshi = Moshi.Builder()
+      .addLast(KotlinJsonAdapterFactory())
+      .build()
+  }
+}
