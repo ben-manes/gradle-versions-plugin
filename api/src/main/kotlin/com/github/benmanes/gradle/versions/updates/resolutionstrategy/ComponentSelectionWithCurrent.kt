6365c48e39f995ba5d702e08ab2ae45b0d0016ae
Jared Burrows <jaredsburrows@gmail.com>
2022-05-06 20:55:09 -0400

    more kotlin (#631)

--- api/src/main/kotlin/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentSelectionWithCurrent.kt
+++ api/src/main/kotlin/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentSelectionWithCurrent.kt
@@ -3,8 +3,8 @@ package com.github.benmanes.gradle.versions.updates.resolutionstrategy
 import org.gradle.api.artifacts.ComponentSelection
 
 class ComponentSelectionWithCurrent @JvmOverloads constructor(
-  val currentVersion: String?,
   private val delegate: ComponentSelection,
+  val currentVersion: String?,
 ) : ComponentSelection by delegate {
 
   override fun toString(): String {
