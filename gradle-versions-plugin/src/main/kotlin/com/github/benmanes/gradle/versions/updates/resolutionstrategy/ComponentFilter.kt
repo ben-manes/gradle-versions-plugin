a247860c9f11658c6d90482899954e5f491d2d35
Víctor J. García <vjgarciaw@gmail.com>
2022-09-11 22:42:57 +0100

    Make ComponentFilter interface functional
    This enables the use of Kotlin's trailing lambda syntax in the `rejectVersionIf` API.

--- gradle-versions-plugin/src/main/kotlin/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentFilter.kt
+++ gradle-versions-plugin/src/main/kotlin/com/github/benmanes/gradle/versions/updates/resolutionstrategy/ComponentFilter.kt
@@ -3,6 +3,6 @@ package com.github.benmanes.gradle.versions.updates.resolutionstrategy
 import org.gradle.api.HasImplicitReceiver
 
 @HasImplicitReceiver
-interface ComponentFilter {
+fun interface ComponentFilter {
   fun reject(candidate: ComponentSelectionWithCurrent): Boolean
 }
