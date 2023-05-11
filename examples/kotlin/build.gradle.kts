d890b369fdd9faffd20ecafa1c3dae29e01adef9
Illia Achour <ilyaachour@gmail.com>
2023-05-11 18:44:59 +0200

    Replace deprecated toUpperCase() kotlin.text calls (#779)
    * Replace deprecated toUpperCase() in README.md
    
    * Replace deprecated toUpperCase() in sample build.gradle.kts

--- examples/kotlin/build.gradle.kts
+++ examples/kotlin/build.gradle.kts
@@ -30,7 +30,7 @@ configurations {
 }
 
 fun String.isNonStable(): Boolean {
-  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { toUpperCase().contains(it) }
+  val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { uppercase().contains(it) }
   val regex = "^[0-9,.v-]+(-r)?$".toRegex()
   val isStable = stableKeyword || regex.matches(this)
   return isStable.not()
