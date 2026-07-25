59ea8f4af60899b97436eb9b90a87e8ffe0b9333
Ian Brandt <ian@ianbrandt.com>
2026-07-25 12:26:29 -0700

    fix: deactivate dependency locking on the plugin's own configurations (fixes #781)

---
+++ gradle-versions-plugin/src/test/groovy/com/github/benmanes/gradle/versions/DependencyLockingSpec.groovy
@@ -0,0 +1,56 @@
+package com.github.benmanes.gradle.versions
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Issue
+import spock.lang.Specification
+
+final class DependencyLockingSpec extends Specification {
+  @Rule final TemporaryFolder testProjectDir = new TemporaryFolder()
+
+  @Issue('https://github.com/ben-manes/gradle-versions-plugin/issues/781')
+  def 'Show updates for a build using strict dependency locking'() {
+    given:
+    def mavenRepoUrl = getClass().getResource('/maven/').toURI()
+    def buildFile = testProjectDir.newFile('build.gradle')
+    buildFile <<
+      """
+        plugins {
+          id 'java-library'
+          id 'io.github.ben-manes.versions'
+        }
+
+        repositories {
+          maven {
+            url '${mavenRepoUrl}'
+          }
+        }
+
+        dependencyLocking {
+          lockMode = LockMode.STRICT
+        }
+
+        configurations.all {
+          resolutionStrategy.activateDependencyLocking()
+        }
+
+        dependencies {
+          api 'com.google.inject:guice:2.0'
+        }
+      """.stripIndent()
+
+    when:
+    def result = GradleRunner.create()
+      .withProjectDir(testProjectDir.root)
+      .withArguments('dependencyUpdates')
+      .withPluginClasspath()
+      .build()
+
+    then:
+    result.output.contains('com.google.inject:guice [2.0 -> 3.1]')
+    result.task(':dependencyUpdates').outcome == SUCCESS
+  }
+}
